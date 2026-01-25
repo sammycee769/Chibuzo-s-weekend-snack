06dd53e8ce9168256aa4dc6f7c2ff46072d13e0b
chris <samuelcristian769@gmail.com>
2026-01-25 18:46:20 +0100

    second

--- BaseConversion/BaseConverter.java
+++ BaseConversion/BaseConverter.java
@@ -18,5 +18,31 @@ return 0;
     }
     return decimalResult;
 }
+public static String toBinary(int decimal){
+if(decimal<0){
+decimal = decimal * -1;
+}
+if(decimal==0){
+return "0";
+}
+int division;
+int multiple;
+String remainder="";
+String numb = "";
 
+while(decimal != 0){
+    division = decimal%2;
+    multiple = decimal / 2;
+    decimal = multiple;
+//    System.out.print(division);
+
+    numb+= division + "";
+}
+
+for(int count=numb.length()-1; count>=0;count--){
+remainder+=numb.charAt(count);
+}
+
+return remainder;
+}
 }
