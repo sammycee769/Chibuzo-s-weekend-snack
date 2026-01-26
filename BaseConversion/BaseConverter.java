99007c501798ea246be2b59e7d72571424669b95
chris <samuelcristian769@gmail.com>
2026-01-26 08:33:05 +0100

    final

--- BaseConversion/BaseConverter.java
+++ BaseConversion/BaseConverter.java
@@ -2,47 +2,53 @@ public class BaseConverter{
 public static void main(String...args){
 System.out.println(toDecimal(101));
 }
-public static int toDecimal(int binary){
-if(binary<0){
-binary *= -1;
+public static String convertToBase(String number, int initialBase, int finalBase){
+    int decimal = toDecimal(number,initialBase);
+    String result = toOtherBase(decimal,finalBase);
+return result;
 }
-if(binary==0){
-return 0;
-}
-    String decimalNumber = binary+ "";
-    int decimalResult = 0;
+public static int toDecimal(String number int base){
+int result = 0;
+int powerOf = 1;
+char character;
+int digit;
 
-    for(int count = 0;count<decimalNumber.length();count++){    
-        decimalResult += (binary%10)*Math.pow(2,count);
-        binary/=10;
+    for(int count = number.length()-1; count>=0; count--){    
+        character = number.charAt(count);
+
+        if(character >= '0' && character <= '9'){
+            digit = character - '0';
+        }else{
+            digit = character - 'A' + 10
+        }
+        result += digit * powerOf;
+        powerOf *= base;
+        } 
+return decimal;
+}
+public static String toOtherBase(int decimal, int base){
+String result = "";
+int remainder;
+    
+    if(decimal == 0){
+        return "0";
     }
-    return decimalResult;
-}
-public static String toBinary(int decimal){
-if(decimal<0){
-decimal = decimal * -1;
-}
-if(decimal==0){
-return "0";
-}
-int division;
-int multiple;
-String remainder="";
-String numb = "";
 
-while(decimal != 0){
-    division = decimal%2;
-    multiple = decimal / 2;
-    decimal = multiple;
-//    System.out.print(division);
+    while(decimal != 0){
+        remainder = decimal % base;
+        if(remainder < 10){
+            digit = (char)(remainder + '0');
+        }else{
+            digit = (char)(remainder - 10 + 'A');
+        }
+        result = digit + result;
+        decimal /= base;
+    }
 
-    numb+= division + "";
+return result;
 }
 
-for(int count=numb.length()-1; count>=0;count--){
-remainder+=numb.charAt(count);
 }
 
-return remainder;
-}
-}
+
+
